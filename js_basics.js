c76fc18431a77ccfcaafa128a9daf55e88058f06
Emmanuel Juma <emmanuel.juma@smartapplicationsgroup.com>
2025-05-26 09:02:06 +0300

    initial commit

---
+++ js_basics.js
@@ -0,0 +1,74 @@
+//Question 1: Correct Syntax
+console.log("Fix the syntax errors in the following code:");
+
+var name = "Ali
+console.log(name)
+
+
+
+//Question 2: console.log()
+console.log("Write code that logs the following output exactly as shown,Use console.log() only:\n"); 
+Name: Sarah
+Age: 25
+Student: true
+
+//Question 3: Comments
+console.log("Rewrite the code below to include:\n");
+// Rewrite the code below to include:
+// - A single-line comment above the first line
+// - A multi-line comment explaining what the program does
+
+var score = 90;
+console.log("Score is", score);
+
+
+
+//Question 4: Variables
+console.log("Question 4: Variables \n");
+// Declare two variables using let:
+// - One should hold your city name as a string
+// - One should hold your age as a number
+// Then print both using a single console.log() statement.
+
+
+//Question 5: Data Types
+console.log("Question 5: Data Types\n");
+// Write a function identifyTypes() that declares 5 variables of different data types:
+// - string
+// - number
+// - boolean
+// - undefined
+// - object
+
+//Question 6: Type Conversion
+console.log("Question 6: Type Conversion\n");
+// Declare a variable numStr = "123" (string)
+// Convert it to a number and store in numVal
+// Print both values and their types.
+
+
+//Question 7: Type Conversion
+console.log("Question 7: Type Conversion\n");
+// Start with a number score = 99
+// Convert it to a string
+// Print both values and their types
+
+//Question 8: Implicit Type Conversion
+console.log("Question 8: Implicit Type Conversion\n");
+// Write code that demonstrates JavaScript's implicit type conversion by adding a number and a string.
+// Print the result and its type.
+
+//Question 9: Use of 'use strict'
+console.log("Question 9: Use of 'use strict'\n");
+// Write a code snippet that uses 'use strict' mode.
+// Inside it, try assigning a value to an undeclared variable and observe what happens (use
+// console.log() to explain).
+
+
+
+//Question 10: Reserved Keywords
+console.log("Question 10: Reserved Keywords\n");
+
+// Try declaring a variable with the name function.
+// - Observe what happens and comment on the error.
+// - Then, fix the issue by using a valid variable name
