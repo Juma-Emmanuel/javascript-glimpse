2f42ddb6a1a69e9865ccbfc0c974d7e2d3673e55
Emmanuel Juma <emmanuel.juma@smartapplicationsgroup.com>
2025-05-27 16:32:23 +0300

    runner

--- runner.js
+++ runner.js
@@ -1,7 +1,3 @@
-var x = 5;
-console.log(x); // it prints 5 as it is declared in the code
-y = 5;
-console.log(y, typeof y);
-function typeof() {}
-
-
+var a = 30;
+var b = 40;
+console.log((a = b));
