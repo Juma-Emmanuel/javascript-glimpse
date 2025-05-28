bd0597481867bba1eae1444566b591ff107a72fd
John Maina <jm922727@gmail.com>
2025-05-28 14:16:56 +0300

    ggg

--- runner.js
+++ runner.js
@@ -1,10 +1,3 @@
-<<<<<<< HEAD
-var x = 10;
-var y = 20;
-
-console.log((x * y < 10)  || (x + y) > x);
-=======
 var a = 30;
 var b = 40;
 console.log((a = b));
->>>>>>> 4ba4c14bdb6d8eea819b209ddcea51ff583d25a2
